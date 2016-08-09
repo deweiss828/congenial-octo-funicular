24c8d4aed2ef3a31ff4de2be4e84dd735a20b36a
Katherine Mello <katherine.mello8@gmail.com>
2016-08-09 12:27:56 -0400

    form for submitting new blog posts

--- index.js
+++ index.js
@@ -24,12 +24,40 @@ var BlogItem = React.createClass({
 	}
 })
 
-var blogElement = React.createElement(BlogItem, {
-	title: 'My week at camp',
-	date: new Date(2016, 08, 09),
-	content: 'I will take back appreciation from camp.',
-	imgUrl: './camp2016.jpg'
+var BlogForm = React.createClass({
+	propTypes:{
+		blog: React.PropTypes.object.isRequired
+	},
+	render: function() {
+		return (
+			React.createElement('form', {},
+				React.createElement('input', {htmlFor: 'title', placeholder: 'Title', value: this.props.blog.title}),
+				React.createElement('input', {htmlFor: 'date', placeholder: 'Date', value: this.props.blog.date}),
+				React.createElement('textarea', {htmlFor:'content', placeholder: 'Content', value: this.props.blog.content}),
+				React.createElement('input', {htmlFor:'imgUrl', placeholder: 'Image Url', value: this.props.blog.imgUrl}),
+				React.createElement('button', {type: 'submit'})
+			)
+		)
+	}
 })
 
 
-ReactDOM.render(blogElement, document.getElementById('react-app'))
+var firstBlog ={
+	title: 'My week at camp',
+	date: new Date(2016, 08, 09),
+	content: 'I will take back appreciation from camp.',
+	imgUrl: './images/camp2016.jpg'
+}
+
+
+var rootElement = React.createElement('div', {},
+		React.createElement(BlogItem,firstBlog),
+		React.createElement(BlogForm, {blog:{title: '',
+			date: '',
+			content: '',
+			imgUrl: ''
+			}
+		})
+	)
+
+ReactDOM.render(rootElement, document.getElementById('react-app'))
