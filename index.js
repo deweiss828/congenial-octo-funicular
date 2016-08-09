e504c8a0881f90e3abe99d8196abd23dcc151ae2
Katherine Mello <katherine.mello8@gmail.com>
2016-08-09 12:03:23 -0400

    creates blog class

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-var rootElement = React.createElement('div', {},
-		React.createElement('h2', {}, 'Title'),
-		React.createElement('h4', {},
-			'Date'),
-		React.createElement('p', {}, 'This is the content'),
-		React.createElement('img', {src:'./nedspoint.png'})
-	)
 
-ReactDOM.render(rootElement, document.getElementById('react-app'))
 
+
+var BlogItem = React.createClass({
+	propTypes: {
+		title: React.PropTypes.string.isRequired,
+
+		date: React.PropTypes.instanceOf(Date).isRequired,
+		content: React.PropTypes.string.isRequired,
+		imgUrl: React.PropTypes.string
+	},
+	render: function() {
+		return (
+			React.createElement('div', {},
+				React.createElement('h2', {}, this.props.title),
+				React.createElement('h4', {},
+					this.props.date.toDateString()),
+				React.createElement('p', {}, this.props.title),
+				React.createElement('img', {
+					src: this.props.imgUrl
+				})
+			)
+		)
+	}
+})
+
+var blogElement = React.createElement(BlogItem, {
+	title: 'My week at camp',
+	date: new Date(2016, 08, 09),
+	content: 'I will take back appreciation from camp.',
+	imgUrl: './camp2016.jpg'
+})
+
+
+ReactDOM.render(blogElement, document.getElementById('react-app'))
