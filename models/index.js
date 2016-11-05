a9b44bea8fddcb15085deb943329e562f44f6be0
Katherine Mello <katherine.mello8@gmail.com>
2016-11-05 18:44:22 -0400

    remove models II

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/myOwnWebsite');
-//mongod is its own process that communicates with filesystem
-//localhost= 127.01.01
+// mongoose.connect('mongodb://localhost:27017/myOwnWebsite');
+// //mongod is its own process that communicates with filesystem
+// //localhost= 127.01.01
 
-mongoose.connection.on('error',function(){
-	console.error(err);
-})
+// mongoose.connection.on('error',function(){
+// 	console.error(err);
+// })
 
-module.exports = {
-	Photograph: require('./photograph')
-};
+// module.exports = {
+// 	Photograph: require('./photograph')
+// };
--- models/photograph.js
+++ models/photograph.js
@@ -1,24 +1,24 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
-var photographSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	description: {
-		type: String
-	},
-	image: {
-		//will store path to image here
-		type: String,
-		required: true
-	},
-	date: {
-		type: Date
-	}
-});
+// var photographSchema = new mongoose.Schema({
+// 	title: {
+// 		type: String,
+// 		required: true,
+// 		unique: true
+// 	},
+// 	description: {
+// 		type: String
+// 	},
+// 	image: {
+// 		//will store path to image here
+// 		type: String,
+// 		required: true
+// 	},
+// 	date: {
+// 		type: Date
+// 	}
+// });
 
-var Photograph = mongoose.model('Photograph', photographSchema);
+// var Photograph = mongoose.model('Photograph', photographSchema);
 
-module.exports = Photograph;
+// module.exports = Photograph;
