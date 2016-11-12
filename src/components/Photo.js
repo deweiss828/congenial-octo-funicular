d76a0ab5a90eef01e4bd4e94db301c27f3bf6dc0
Katherine Mello <katherine.mello8@gmail.com>
2016-11-12 15:00:34 -0500

    initial photo component

---
+++ src/components/Photo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import '../styles/photo.scss'
+
+const Photo = () => (
+	<figure id="photo-container">
+		<img src="http://imgur.com/XDvNck4.png"/>
+		<figcaption>Manhattan</figcaption>
+	</figure>
+)
+
+export default Photo
