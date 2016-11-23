56720e310a61cf886202a1e5b671bffd9a36662f
Katherine Mello <katherine.mello8@gmail.com>
2016-11-22 20:14:05 -0500

    configuration for local and production db

--- db/db.js
+++ db/db.js
@@ -2,12 +2,12 @@
 const path = require('path');
 const chalk = require('chalk');
 const Sequelize = require('sequelize');
-const DATABASE_URI = require(path.join(__dirname, '../config')).DATABASE_URI;
+const DATABASE_URL = require(path.join(__dirname, '../config')).DATABASE_URL;
 
 console.log(chalk.yellow('Opening connection to PostgreSQL'));
 
 // create the database instance
-module.exports = new Sequelize(DATABASE_URI, {
+module.exports = new Sequelize(DATABASE_URL, {
   logging: true, // set to console.log to see the raw SQL queries
   // native: true // lets Sequelize know we can use pg-native for ~30% more speed
 });
